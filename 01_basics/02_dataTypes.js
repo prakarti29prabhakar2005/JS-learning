6fd01b72ff69f15a408dd43f6c9f30aceea27bfc
prakarti29prabhakar2005 <129519489+prakarti29prabhakar2005@users.noreply.github.com>
2023-11-24 14:01:27 +0000

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) //We are using nodejs, not browser
+//important documents for javascript : tc39, mdm
+
+let Name = "Prakarti"
+let age = 18
+let isLoggedIn = false
+//primitive data types : number, bigint, string, boolean, nuii(it is a standalone value), undefined, symbol(it is used when we ned to find the unique)
+
+//object
+
+console.log(typeof Name);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
