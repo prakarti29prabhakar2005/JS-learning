651e9b175b5765e349da77314f8bcbdd1942179c
prakarti29prabhakar2005 <129519489+prakarti29prabhakar2005@users.noreply.github.com>
2023-12-06 20:17:30 +0000

    Switch case

---
+++ 04_basics_controlFlow/02_switch.js
@@ -0,0 +1,17 @@
+const month = 2
+
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("February")
+        break;
+    case 3:
+        console.log("March")
+        break;    
+
+    default:
+        console.log("Not from the starting three months")
+        break;
+}
